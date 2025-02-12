0013acc04265cf5805bc1614f73df2dabe960338
MatiasJurgenson <jurgenson.matias@gmail.com>
2025-02-12 18:04:59 +0200

    tahendus route

--- main.py
+++ main.py
@@ -11,14 +11,37 @@ download('estwordnet_2020-06-30')
 
 app = Flask(__name__)
 
-@app.route('/api')
-def api_data():
+def sõne_to_synod(sõne):
+    text = Text(sõne).tag_layer()
+    lemma =  text.words.lemma[0][0]
+    synod = Wordnet()[lemma]
+    return synod
+
+# teema genereerimise jaoks
+@app.route('/teema')
+def teema_data():
     sõne = request.args.get('sone')
-    synod = Wordnet()[sõne]
+    synod = sõne_to_synod(sõne)
+
     #synod = Wordnet(version='2.3.2')[sõne]
     teema = [s.name.split('.') for s in synod]
     
     data = {'sõnastik': teema}
+    return jsonify(data)
+
+@app.route('/tahendus')
+def tahendus_data():
+    sõne = request.args.get('sone')
+    synod = sõne_to_synod(sõne)
+    
+    tähendused = []
+    for asi in synod:
+        tähendused.append(asi.definition)
+
+    if len(tähendused) == 0:
+        tähendused.append("Tähendust ei leitud")
+
+    data = {'sõnastik': tähendused}
     return jsonify(data)
 
 #debugger 
